4cab6c534d4cc74ddcd677a731aadbcee187ee43
gheber <gheber@hdfgroup.org>
2016-02-09 15:19:09 -0600

    H5TTest using hid_t updates.

--- UnitTests/H5TTest/H5Tcommit_anon.cs
+++ UnitTests/H5TTest/H5Tcommit_anon.cs
@@ -17,8 +17,13 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HDF.PInvoke;
 
-using hid_t = System.Int32;
 using hsize_t = System.UInt64;
+
+#if HDF5_VER1_10
+using hid_t = System.Int64;
+#else
+using hid_t = System.Int32;
+#endif
 
 namespace UnitTests
 {
