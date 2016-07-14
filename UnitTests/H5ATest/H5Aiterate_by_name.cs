dfa06ec2a47dfc14f507eb7bbd079647d91de087
Haymo Kutschbach <h.kutschbach@ilnumerics.net>
2016-07-14 22:55:12 +0200

    Attempt to fix issue #35

--- UnitTests/H5ATest/H5Aiterate_by_name.cs
+++ UnitTests/H5ATest/H5Aiterate_by_name.cs
@@ -21,6 +21,8 @@ using HDF.PInvoke;
 
 using herr_t = System.Int32;
 using hsize_t = System.UInt64;
+using System.Linq;
+using System.Text;
 
 #if HDF5_VER1_10
 using hid_t = System.Int64;
@@ -49,18 +51,32 @@ namespace UnitTests
                m_space_scalar);
             Assert.IsTrue(att >= 0);
             Assert.IsTrue(H5A.close(att) >= 0);
-
             ArrayList al = new ArrayList();
-            GCHandle hnd = GCHandle.Alloc(al);
-            IntPtr op_data = (IntPtr)hnd;
             hsize_t n = 0;
-            // the callback is defined in H5ATest.cs
-            H5A.operator_t cb = DelegateMethod;
+
+            // if we define the callback as lambda we can simple capture the array list
+            H5A.operator_t cb = (hid_t location_id, IntPtr attr_name, ref H5A.info_t ainfo, IntPtr op_data
+            ) => {
+                Assert.IsTrue(op_data.ToInt64() == -99); 
+                int len = 0;
+                while (Marshal.ReadByte(attr_name, len) != 0) { ++len; }
+                byte[] buf = new byte[len];
+                Marshal.Copy(attr_name, buf, 0, len);
+                al.Add(Encoding.UTF8.GetString(buf));
+                return 0;
+            };
+
             Assert.IsTrue(H5A.iterate_by_name(m_v2_test_file, ".",
                 H5.index_t.NAME, H5.iter_order_t.NATIVE, ref n, cb,
-                op_data) >= 0);
-            // we should have 3 elements in the array list
+                // the op_data argument is simply tested as a constant
+                new IntPtr(-99)) >= 0);
+            // we should have 3 elements in the array list [now obsolete]
             Assert.IsTrue(al.Count == 3);
+            Assert.IsFalse(al.ToArray().Any(s => s == null || String.IsNullOrEmpty(s.ToString())));
+            // let's be specific
+            Assert.IsTrue(al[0].ToString() == "IEEE_F32BE"); 
+            Assert.IsTrue(al[1].ToString() == "IEEE_F64BE"); 
+            Assert.IsTrue(al[2].ToString() == "NATIVE_B8");
 
             att = H5A.create(m_v0_test_file, "IEEE_F32BE",
                 H5T.IEEE_F32BE, m_space_scalar);
@@ -77,15 +93,18 @@ namespace UnitTests
             Assert.IsTrue(att >= 0);
             Assert.IsTrue(H5A.close(att) >= 0);
 
-            al.Clear();
+            al.Clear(); 
             n = 0;
             Assert.IsTrue(H5A.iterate_by_name(m_v0_test_file, ".",
                 H5.index_t.NAME, H5.iter_order_t.NATIVE, ref n, cb,
-                op_data) >= 0);
-            // we should have 3 elements in the array list
+                new IntPtr(-99)) >= 0);
+            // we should have 3 elements in the array list [now obsolete]
             Assert.IsTrue(al.Count == 3);
-
-            hnd.Free();
+            Assert.IsFalse(al.ToArray().Any(s => s == null || String.IsNullOrEmpty(s.ToString())));
+            // let's be specific
+            Assert.IsTrue(al[0].ToString() == "IEEE_F32BE");
+            Assert.IsTrue(al[1].ToString() == "IEEE_F64BE");
+            Assert.IsTrue(al[2].ToString() == "NATIVE_B8");
         }
 
         [TestMethod]
