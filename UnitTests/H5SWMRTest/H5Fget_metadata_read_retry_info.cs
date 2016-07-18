d7209cfcaf236c6ef1b08bbe77ac7867a99f5fed
gheber <gheber@hdfgroup.org>
2016-07-18 08:54:10 -0500

    Added tests for metadata read functions.
    We need to resolve issue #68 before the H5Fget_metadata_read_retry_info
    test gets a chance.

---
+++ UnitTests/H5SWMRTest/H5Fget_metadata_read_retry_info.cs
@@ -0,0 +1,42 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HDF.PInvoke;
+
+using herr_t = System.Int32;
+
+#if HDF5_VER1_10
+
+using hid_t = System.Int64;
+
+namespace UnitTests
+{
+    public partial class H5SWMRTest
+    {
+        [TestMethod]
+        public void H5Fget_metadata_read_retry_infoTestSWMR1()
+        {
+            H5F.retry_info_t info = new H5F.retry_info_t();
+            Assert.IsTrue(
+                H5F.get_metadata_read_retry_info(m_v3_test_file_swmr,
+                ref info) >= 0);
+        }
+    }
+}
+
+#endif
---
+++ UnitTests/H5SWMRTest/H5Pget_metadata_read_attempts.cs
@@ -0,0 +1,61 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HDF.PInvoke;
+
+using herr_t = System.Int32;
+
+#if HDF5_VER1_10
+
+using hid_t = System.Int64;
+
+namespace UnitTests
+{
+    public partial class H5SWMRTest
+    {
+        [TestMethod]
+        public void H5Fget_metadata_read_attemptsTestSWMR1()
+        {
+            hid_t fapl = H5F.get_access_plist(m_v3_test_file_swmr);
+            Assert.IsTrue(fapl >= 0);
+
+            uint attempts = 0;
+            Assert.IsTrue(
+                H5P.get_metadata_read_attempts(fapl, ref attempts) >= 0);
+            Assert.IsTrue(attempts > 0);
+
+            Assert.IsTrue(H5P.close(fapl) >= 0);
+        }
+
+        [TestMethod]
+        public void H5Fget_metadata_read_attemptsTestSWMR2()
+        {
+            hid_t fapl = H5F.get_access_plist(m_v3_test_file_no_swmr);
+            Assert.IsTrue(fapl >= 0);
+
+            uint attempts = 0;
+            Assert.IsTrue(
+                H5P.get_metadata_read_attempts(fapl, ref attempts) >= 0);
+            Assert.IsTrue(attempts > 0);
+
+            Assert.IsTrue(H5P.close(fapl) >= 0);
+        }
+    }
+}
+
+#endif
