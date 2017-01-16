f17a2bfd781da6c5a60569080e7f27eb94022e38
Jan Wosnitza <j.wosnitza@ahm.co.at>
2017-01-16 15:13:07 +0100

    pack NuGet with Paket & Fake

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -26,20 +26,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("161e6943-58a3-4643-bf68-113ee2b2df92")]
 #endif
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-#if HDF5_VER1_10
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.4")]
-#else
-[assembly: AssemblyVersion("1.8.17.0")]
-[assembly: AssemblyFileVersion("1.8.17.8")]
-#endif
+// Updated by build.fsx:
+[assembly: AssemblyVersion("1.10.0.4")]
---
+++ RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+##### 1.10.0.4
+* Added missing predefined datatypes (INTEL, MIPS, VAX)
+* Added the CMake configuration files for building the native dependnecies
+* The native dependencies included were built as RelWithDebugInfo (PDBs are included)
+##### 1.8.17.8
+* Added missing predefined datatypes (INTEL, MIPS, VAX)
+* Added the CMake configuration files for building the native dependnecies
+* The native dependencies included were built as RelWithDebugInfo (PDBs are included)
