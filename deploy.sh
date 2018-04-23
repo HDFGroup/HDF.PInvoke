0c01ad3810d129d157656d8939e2e73a9a517656
Sebastian Urban <surban@surban.net>
2018-04-24 00:36:44 +0200

    CI

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Push NuGet packages.
+rm -f "bin/Release/*.symbols.nupkg"
 dotnet nuget push -s https://www.myget.org/F/coreports/api/v2/package -k $MYGETKEY 'bin/Release/*.nupkg' || true
