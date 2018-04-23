a5c090009be8234276d4f8d825485b7d1da3efde
Sebastian Urban <surban@surban.net>
2018-04-23 23:11:56 +0200

    Add NuGet package test project.

---
+++ NugetTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using HDF.PInvoke;
+
+namespace NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting HDF5 NuGet package consumption test...");
+
+            var handle = H5F.create("test.h5", H5F.ACC_TRUNC);
+            Debug.Assert(handle >= 0);
+
+            var retval = H5F.close(handle);
+            Debug.Assert(retval >= 0);
+
+            File.Delete ("test.h5");
+
+            Console.WriteLine("HDF5 NuGet package consumption test successful.");
+        }
+    }
+}
