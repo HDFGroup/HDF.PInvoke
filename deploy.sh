ce5daecc3ea78dedb0e4392faf958b58e43a2f5b
Sebastian Urban <surban@surban.net>
2018-04-23 23:41:09 +0200

    build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Push NuGet packages.
-dotnet nuget push -s https://www.myget.org/F/coreports/api/v2/package -k $MYGETKEY 'bin/Release/*.nupkg' || true
+dotnet nuget push -s https://www.myget.org/F/coreports/api/v2/package -k $MYGETKEY "bin/Release/*.nupkg" || true
