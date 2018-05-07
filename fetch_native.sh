85d7787a73f9ec9a1a8aec56046074db33d525ec
Sebastian Urban <surban@surban.net>
2018-05-07 21:12:23 +0200

    OSX native lib

--- fetch_native.sh
+++ fetch_native.sh
@@ -5,9 +5,13 @@ NATIVE_SRC="$(cat native_src.txt)"
 set -e
 pushd $(dirname "${BASH_SOURCE[0]}")/native/HDF5_1.10
 wget -q $NATIVE_SRC/hdf5_linux.zip
+wget -q $NATIVE_SRC/hdf5_mac.zip
 wget -q $NATIVE_SRC/hdf5_windows.zip
 unzip hdf5_linux.zip
+unzip hdf5_mac.zip
 unzip hdf5_windows.zip
-rm -f hdf5_linux.zip hdf5_windows.zip
+rm -f hdf5_linux.zip 
+rm -f hdf5_mac.zip
+rm -f hdf5_windows.zip
 popd
 
