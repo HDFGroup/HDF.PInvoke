c26214f85585859d11d8ed9921aff62e3a6d09f1
Sebastian Urban <surban@surban.net>
2018-05-07 20:13:18 +0200

    Remove binaries from repository and download during build.

---
+++ fetch_native.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = "Stop"
+
+$NATIVE_SRC = Get-Content native_src.txt -Raw
+
+pushd "$PSScriptRoot/native/HDF5_1.10"
+Invoke-WebRequest "$NATIVE_SRC/hdf5_linux.zip" -OutFile hdf5_linux.zip
+Invoke-WebRequest "$NATIVE_SRC/hdf5_windows.zip" -OutFile hdf5_windows.zip
+7z x hdf5_linux.zip
+7z x hdf5_windows.zip
+rm hdf5_linux.zip
+rm hdf5_windows.zip
+popd
+
